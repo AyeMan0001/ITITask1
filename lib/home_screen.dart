052db031a1b7df15845cc078f081ee6eb58cfb8d
AyeMan001 <91797379+AyeMan001@users.noreply.github.com>
2023-07-19 00:50:34 +0300

    Task3

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -5,11 +6,14 @@ import 'package:untitled/my_button.dart';
 import 'package:untitled/second_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 
 
 class  HomePage extends StatefulWidget {
   const HomePage({super.key});
+
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -18,6 +22,8 @@ class  HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController emailControler = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(appBar: AppBar(
@@ -25,62 +31,75 @@ class _HomePageState extends State<HomePage> {
       title: Text("Login Page"),
       centerTitle: true,
     ),
-        body : Form(
-      key: _formKey,
-      child:  Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextFormField( controller: emailControler,decoration: new InputDecoration(
-                hintText: 'Email'
-            ),
-              validator: (value){
-              if(value!.isEmpty){
-                return  "Please enter your email";
-              }
-              },
-            ),
+        body:
+
+        Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: emailControler, decoration: new InputDecoration(
+                    hintText: 'Email'
+                ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Please enter your email";
+                    }
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(controller: passwordController,
+                  decoration: new InputDecoration(
+                      hintText: 'Password'
+                  ),
+                  validator: (value) {
+                    if (value!.length < 7) {
+                      return "Please enter a password that contains more than 7 chars";
+                    }
+                  },
+
+                ),
+              ),
+
+              InkWell(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    signinUsingFirebase(emailControler.text, passwordController.text);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => SecondScreen(
+                            email: emailControler.text,
+                          )),
+                    );
+                  }else{
+                    emailControler.clear();
+
+                  }
+                },
+                child: MyButton(label: "Login"),),
+
+
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: TextFormField( decoration: new InputDecoration(
-                hintText: 'Password'
-            ),
-              validator: (value){
-                if(value!.length < 7){
-                  return  "Please enter a password that contains more than 7 chars";
-                }
-              },
-
-            ),
-          ),
-
-          InkWell(
-            onTap:(){
-              if(_formKey.currentState!.validate()){
-                Navigator.push(context,
-                MaterialPageRoute(builder: (context)=>
-                SecondScreen(
-                  email: emailControler.text,
-
-
-                )
-                ));
-              }
-            },
-            child: MyButton(label: "Login"),),
-
-
-
-
-
-
-
-
-        ],
-      ),
-    )
+        )
 
     );
+  }
+  saveEmail(String email) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString("email", email);
+  }
+
+  signinUsingFirebase(String email, String password) async {
+    UserCredential userCredential =
+    await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+    final user = userCredential.user;
+    print(user?.uid);
+    saveEmail(user!.email!);
   }
 }
--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -70,4 +71,6 @@ class _SecondScreenState extends State<SecondScreen> {
         )
     );
   }
-    }
+
+
+}
